d6d6b4341084ddec77bc607aa7e33054f8ca02fe
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-01-28 20:35:33 +0100

    added git

--- configuration.nix
+++ configuration.nix
@@ -67,6 +67,7 @@
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    git
     blesh
     atuin
     sl

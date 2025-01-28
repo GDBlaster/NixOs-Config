9c139f08574a5468cf76ac2c7e3ae4db608db70f
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-01-28 20:33:29 +0100

    Activated Flakes

--- configuration.nix
+++ configuration.nix
@@ -6,20 +6,20 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
+  # Enable Flakes
+  nix.settings.experimental-features = ["nix-commands" "flakes"];
+
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos-vm"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;

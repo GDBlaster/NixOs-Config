7b8c8e5a171e81b53f117f07ea6274be854cd680
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-01-29 00:13:29 +0100

    for real this time

--- modules/default.nix
+++ modules/default.nix
@@ -5,7 +5,7 @@
 
   ];
 
-  environment.systemPackages = with nixpkgs; [
+  environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     curl

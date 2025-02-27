060116f089863f313b7599ee37a9a982982a4237
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-02-27 16:26:51 +0100

    removed GRM

--- flake.nix
+++ flake.nix
@@ -4,27 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
 
-    flake-utils.url = "github:numtide/flake-utils";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    git-repo-manager = {
-      url = "github:hakoerber/git-repo-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
   outputs =
     { nixpkgs, home-manager, ... }@inputs:
     {
-      pkgs = import inputs.nixpkgs {
-        overlays = [ inputs.git-repo-manager.overlays.git-repo-manager ];
-      };
-
       nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };

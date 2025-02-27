d13e3873936be6993987d4fb00cdfa3ca9524b5e
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-02-27 18:34:31 +0100

    nh config + auto cleanup

--- modules/nh/nh.nix
+++ modules/nh/nh.nix
@@ -3,5 +3,16 @@
   environment.systemPackages = with pkgs; [
     nh
   ];
-  
+
+  environment.sessionVariables = {
+    FLAKE = "/etc/nixos";
+  };
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 2d --keep 3";
+    flake = "/etc/nixos";
+  };
+
 }

cb9415549b1a222cab451cf6838e191c5a454521
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-02-28 17:56:15 +0100

    changed hyprland package

--- hosts/nixos-vm/configuration.nix
+++ hosts/nixos-vm/configuration.nix
@@ -35,8 +35,14 @@
   ];
 
   # GUI
-  programs.hyprland.enable = true;
-  # programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.

6282d445ceaf82bec38ca9613d7c6a21ae0684aa
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-03-02 22:48:07 +0100

    dunno how to type

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -4,7 +4,7 @@ lib.mkIf (config.desktop == "gnome") {
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  environment.gnome.excludedPackages = with pkgs ; [
+  environment.gnome.excludePackages = with pkgs; [
     geary
     gnome-weather
   ];

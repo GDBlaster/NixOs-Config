db459f6289b7832986cc2f4ed4005ae82cc314c4
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-03-02 22:44:45 +0100

    gnum

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,1 +1,11 @@
-{ ... }: { }
+{ lib, config, pkgs, ... }: 
+lib.mkIf (config.desktop == "gnome") {
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludedPackages = with pkgs ; [
+    geary
+    gnome-weather
+  ];
+}

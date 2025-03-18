902c9280857caae5ed650f37d8abb8635458ddf5
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-03-18 22:36:07 +0100

    revert unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Main System Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";

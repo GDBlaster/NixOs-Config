346b6a74a28470ac04c69b0aac33fda493013327
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-05-29 02:15:51 +0200

    no work but cleanup anyways

--- modules/home-manager/users/default.nix
+++ modules/home-manager/users/default.nix
@@ -1,8 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
-
-  # Fix an issue with systemd not having nix in its path for user services
-  systemd.user.services.home-manager-auto-upgrade.Service.Environment = [
-    "PATH=${lib.makeSearchPath "bin" [ pkgs.nix ]}"
-  ];
+  # Common stuff for every profile
 }

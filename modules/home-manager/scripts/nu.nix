badaec7d3d8136cce84aa9f217390e4abfbd88c2
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-12-16 10:38:48 +0100

    Feat: nu cleans up on CTRL+C

--- modules/home-manager/scripts/nu.nix
+++ modules/home-manager/scripts/nu.nix
@@ -5,38 +5,60 @@
 }:
 let
   standalone = pkgs.writeShellScriptBin "nu" ''
-    git -C ~/NixOs-Config pull
-    nh home switch -b backup -u
-
-    if [ $? -eq 0 ]; then
-      COMMIT_DATE=$(date +%Y%m%d)
-
-      git -C ~/NixOs-Config add flake.lock
-      git -C ~/NixOs-Config commit -m "$COMMIT_DATE" --only flake.lock
-      git -C ~/NixOs-Config push
-    else
-      echo "Update failed to build changes reverted"
-
-      git -C ~/NixOs-Config checkout -- flake.lock
-      exit 1
-    fi
-  '';
-
-  module = pkgs.writeShellScriptBin "nu" ''
-    git -C /etc/nixos pull
-    nh os switch -u
-    if [ $? -eq 0 ]; then
-
-      COMMIT_DATE=$(date +%Y%m%d)
-    
-      git -C /etc/nixos add flake.lock
-      git -C /etc/nixos commit -m "$COMMIT_DATE" --only flake.lock
-      git -C /etc/nixos push
-    else
+    cleanup() { 
       echo "Update failed to build changes reverted"
 
       git -C /etc/nixos checkout -- flake.lock
       exit 1
+    }
+
+    trap cleanup INT
+    nh home switch -b backup -u
+    git -C /etc/nixos pull --rebase --autostash 
+
+    if [ $? -eq 0 ]; then
+
+      COMMIT_DATE=$(date +%Y%m%d)
+
+      trap - INT
+      git -C /etc/nixos add flake.lock
+      git -C /etc/nixos commit -m "$COMMIT_DATE" --only flake.lock
+      git -C /etc/nixos push
+
+    else
+
+      trap - INT
+      cleanup
+      
+    fi
+  '';
+
+  module = pkgs.writeShellScriptBin "nu" ''
+    cleanup() { 
+      echo "Update failed to build changes reverted"
+
+      git -C /etc/nixos checkout -- flake.lock
+      exit 1
+    }
+
+    trap cleanup INT
+
+    git -C /etc/nixos pull --rebase --autostash 
+    nh os switch -u
+    if [ $? -eq 0 ]; then
+
+      COMMIT_DATE=$(date +%Y%m%d)
+
+      trap - INT
+      git -C /etc/nixos add flake.lock
+      git -C /etc/nixos commit -m "$COMMIT_DATE" --only flake.lock
+      git -C /etc/nixos push
+
+    else
+
+      trap - INT
+      cleanup
+      
     fi
   '';
 

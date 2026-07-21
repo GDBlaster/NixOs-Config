276e05da03908b851f8076c74453de819949ab01
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2026-07-21 15:47:03 +0200

    fixup! Feat: add DDSS2 server

--- modules/arion/stacks/mc-ddss2.nix
+++ modules/arion/stacks/mc-ddss2.nix
@@ -9,7 +9,7 @@
           ports = [ "25565:25565" ];
           environment = {
             TYPE = "AUTO_CURSEFORGE";
-            CF_PAGE_URL =  "https://www.curseforge.com/minecraft/modpacks/dungeons-dragons-and-space-shuttles-2";
+            CF_PAGE_URL = "https://www.curseforge.com/minecraft/modpacks/dungeons-dragons-and-space-shuttles-2";
             CF_API_KEY = "$$2a$$10$$patTU5A9RSaMjVpzE.BXIeeHdcU5kba3GHXpQFfcxXdmR9JLWSrwW";
             VERSION = "1.16.5";
             OPS = "GD_Blaster";
@@ -18,15 +18,20 @@
             MEMORY = "8064M";
             MOTD = "CHEESE";
             ONLINE_MODE = "true";
+            MODRINTH_PROJECTS = ''
+              	      ferrite-core
+                            lithium
+              	      distanthorizons:beta
+              	    '';
             MODRINTH_DOWNLOAD_DEPENDENCIES = "optional";
             USE_MEOWICE_FLAGS = "true";
             SPAWN_PROTECTION = "0";
           };
-          volumes = [ 
+          volumes = [
             "/data/mc-ddss2/:/data"
             "/data/mc-ddss2-mods/:/mods"
           ];
-          
+
         };
       };
     };
